69d3bd3d398f87f58dd51a4203988733cbfdae05
Robin Claes <robin.claes@icapps.com>
2017-05-31 12:44:12 +0200

    remove karma from gulp

--- gulp_tasks/karma.js
+++ gulp_tasks/karma.js
@@ -1,27 +1,27 @@
-process.env.NODE_ENV = 'test';
+// process.env.NODE_ENV = 'test';
 
-const path = require('path');
+// const path = require('path');
 
-const gulp = require('gulp');
-const karma = require('karma');
+// const gulp = require('gulp');
+// const karma = require('karma');
 
-gulp.task('karma:single-run', karmaSingleRun);
-gulp.task('karma:auto-run', karmaAutoRun);
+// gulp.task('karma:single-run', karmaSingleRun);
+// gulp.task('karma:auto-run', karmaAutoRun);
 
-function karmaFinishHandler(done) {
-  return failCount => {
-    done(failCount ? new Error(`Failed ${failCount} tests.`) : null);
-  };
-}
+// function karmaFinishHandler(done) {
+//   return failCount => {
+//     done(failCount ? new Error(`Failed ${failCount} tests.`) : null);
+//   };
+// }
 
-function karmaSingleRun(done) {
-  const configFile = path.join(process.cwd(), 'conf', 'karma.conf.js');
-  const karmaServer = new karma.Server({configFile}, karmaFinishHandler(done));
-  karmaServer.start();
-}
+// function karmaSingleRun(done) {
+//   const configFile = path.join(process.cwd(), 'conf', 'karma.conf.js');
+//   const karmaServer = new karma.Server({configFile}, karmaFinishHandler(done));
+//   karmaServer.start();
+// }
 
-function karmaAutoRun(done) {
-  const configFile = path.join(process.cwd(), 'conf', 'karma-auto.conf.js');
-  const karmaServer = new karma.Server({configFile}, karmaFinishHandler(done));
-  karmaServer.start();
-}
+// function karmaAutoRun(done) {
+//   const configFile = path.join(process.cwd(), 'conf', 'karma-auto.conf.js');
+//   const karmaServer = new karma.Server({configFile}, karmaFinishHandler(done));
+//   karmaServer.start();
+// }
